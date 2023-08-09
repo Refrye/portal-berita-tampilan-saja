cfe37a11299436463ae6e0f7b0adeba58fd997c0
Refrye <ryuunusukeray@gmail.com>
2023-08-10 05:49:12 +0700

    'setting frontend dan backend'

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/react";
+
+export default function Homepage(props) {
+    console.log(props)
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    );
+}
